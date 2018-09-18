8480b2c65eb742ac62f101d2f785b02b1db3a5a0
hasan <hasan@hasanmac>
2018-09-18 02:26:49 -0500

    testproject2 first commit

---
+++ testproj2/src/hello.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+
+public class hello {
+
+    public static void main(String argv[]) {
+	// Complete here with appropriate text
+        System.out.println("Hello world............");
+
+    }
+} // end class hell
